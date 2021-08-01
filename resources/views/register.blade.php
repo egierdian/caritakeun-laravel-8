24faea275c26017a24aecf178fc5745bbcc490db
egierdian <egierdian1@gmail.com>
2021-08-01 10:43:11 +0700

    design login register

--- resources/views/register.blade.php
+++ resources/views/register.blade.php
@@ -1,59 +1,64 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Register</title>
+    <title>Register | Caritakeun</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@300;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('css/styles.css')}}">
 </head>
+
 <body>
     <div class="container">
-        <div class="col-md-4 offset-md-4 mt-5">
-            <div class="card">
+        <div class="col-md-6 offset-md-3 mt-5 col-12">
+            <div class="card shadow-login-register">
                 <div class="card-header">
-                    <h3 class="text-center">Form Register</h3>
+                    <h3 class="text-center">Registrasi</h3>
                 </div>
                 <form action="{{ route('register')}}" method="post">
-                @csrf
-                <div class="card-body">
-                    @if(session('errors'))
+                    @csrf
+                    <div class="card-body">
+                        @if(session('errors'))
                         <div class="alert alert-danger alert-dismissible fade show" role="alert">
                             Something it's wrong:
                             <button type="button" class="close" data-dismiss="alert" aria-label="Close">
                                 <span aria-hidden="true">×</span>
                             </button>
                             <ul>
-                            @foreach ($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                            @endforeach
+                                @foreach ($errors->all() as $error)
+                                <li>{{ $error }}</li>
+                                @endforeach
                             </ul>
                         </div>
-                    @endif
-                    <div class="form-group">
-                        <label for=""><strong>Nama Lengkap</strong></label>
-                        <input type="text" name="name" class="form-control" placeholder="Nama Lengkap">
+                        @endif
+                        <div class="form-group">
+                            <label for=""><strong>Nama Lengkap</strong></label>
+                            <input type="text" name="name" class="form-control" placeholder="Nama Lengkap">
+                        </div>
+                        <div class="form-group">
+                            <label for=""><strong>Email</strong></label>
+                            <input type="text" name="email" class="form-control" placeholder="Email">
+                        </div>
+                        <div class="form-group">
+                            <label for=""><strong>Password</strong></label>
+                            <input type="password" name="password" class="form-control" placeholder="Password">
+                        </div>
+                        <div class="form-group">
+                            <label for=""><strong>Konfirmasi Password</strong></label>
+                            <input type="password" name="password_confirmation" class="form-control" placeholder="Password">
+                        </div>
                     </div>
-                    <div class="form-group">
-                        <label for=""><strong>Email</strong></label>
-                        <input type="text" name="email" class="form-control" placeholder="Email">
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary btn-block">Register</button>
+                        <p class="text-center pt-3">Sudah punya akun? <a href="{{ route('login')}}">Login</a> sekarang!</p>
                     </div>
-                    <div class="form-group">
-                        <label for=""><strong>Password</strong></label>
-                        <input type="password" name="password" class="form-control" placeholder="Password">
-                    </div>
-                    <div class="form-group">
-                        <label for=""><strong>Konfirmasi Password</strong></label>
-                        <input type="password" name="password_confirmation" class="form-control" placeholder="Password">
-                    </div>
-                </div>
-                <div class="card-footer">
-                    <button type="submit" class="btn btn-primary btn-block">Register</button>
-                    <p class="text-center">Sudah punya akun? <a href="{{ route('login')}}">Login</a> sekarang!</p>
-                </div>
                 </form>
             </div>
         </div>
     </div>
 </body>
+
 </html>
